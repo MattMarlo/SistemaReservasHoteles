dbb43fb88112d6316f83361d2b56c1cbfae31f86
Jhon Guanoluisa <estiven.guanoluisa8851@utc.edu.ec>
2026-01-20 16:18:19 -0500

    Resolver conflicto en header.blade.php

--- resources/views/share/header.blade.php
+++ resources/views/share/header.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-          <p>hotel de Estiven</p>
+          <p>hotel de Estiven</p> <!-- para el encavesado -->
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
